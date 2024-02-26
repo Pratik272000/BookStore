ebeb6b61163f964e53c1ecf052e7812d4cd6cd14
Pratik Koli <pkoli4470@gmail.com>
2024-02-26 15:42:49 +0530

    Author entity added and @gette,@setter,@NoArgsConstructor,@AllArgsConstructor added in Book entity

--- BookStore/src/main/java/com/app/entities/Author.java
+++ BookStore/src/main/java/com/app/entities/Author.java
@@ -1,23 +1,24 @@
 package com.app.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
-public class Book {
+@NoArgsConstructor
+public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     String name;
-    String isbn;
+    String email;
+    @OneToMany
+    List<Book> books;
 }
