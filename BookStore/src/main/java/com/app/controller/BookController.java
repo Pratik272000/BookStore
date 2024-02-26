8e49e858d7384e294385810480187ecd4654c358
Pratik Koli <pkoli4470@gmail.com>
2024-02-26 17:48:40 +0530

    test case add for Book service

--- BookStore/src/main/java/com/app/controller/BookController.java
+++ BookStore/src/main/java/com/app/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.app.controller;
 import com.app.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 @Autowired
     BookService bookService;
+@GetMapping("/get")
+    public ResponseEntity<?> getAllBooks(){
+    return ResponseEntity.ok(bookService.getAllBooks());
+}
 
 }
--- BookStore/src/main/java/com/app/service/BookService.java
+++ BookStore/src/main/java/com/app/service/BookService.java
@@ -1,11 +1,18 @@
 package com.app.service;
 
 import com.app.dao.BookDao;
+import com.app.entities.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
     @Autowired
     BookDao bookDao;
+
+    public List<Book> getAllBooks() {
+        return bookDao.findAll();
+    }
 }
